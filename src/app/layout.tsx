835e28aac4ebfa41ffb5c3179e970fcf0120e070
pigee1203-dotcom <pigee1203@gmail.com>
2026-04-08 11:37:51 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "./components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Varna Dashboard",
